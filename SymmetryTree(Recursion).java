ee272829e3054fc0f6079a03058221e7ca8e6114
Hitesh Vats <66768966+hiteshvats-923587599@users.noreply.github.com>
2020-07-28 18:50:52 +0530

    Create SymmetryTree(Recursion).java

---
+++ SymmetryTree(Recursion).java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean isMirror(TreeNode tree1,TreeNode tree2)
+    {
+        if(tree1==null && tree2==null) return true;
+        
+        if(tree1==null || tree2==null) return false;
+        
+    
+        
+return (tree1.val==tree2.val) && (isMirror(tree1.left,tree2.right)) && (isMirror(tree1.right,tree2.left)); 
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        
+        return isMirror(root,root);
+        
+    }
+}
