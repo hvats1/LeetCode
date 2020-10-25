92cac22a51838c6a8d1431f6af1d599155d4fbba
Hitesh Vats <66768966+hiteshvats-923587599@users.noreply.github.com>
2020-10-25 18:37:39 +0530

    Create ThreeSum.java

---
+++ ThreeSum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums); //O(nlogn)
+        
+        List<List<Integer>> ansList = new ArrayList<>();
+        
+        
+        for(int i=0;i<nums.length-2;i++)
+        {
+            if(i==0 || (i>0 && nums[i-1]!=nums[i]))
+            {
+            int low=i+1;
+            int high=nums.length-1;
+            int sum=0-nums[i];
+            
+            while(low<high)
+            {
+                if(nums[low]+nums[high]==sum)
+                {
+                    ansList.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                    
+                while(low<high && nums[low]==nums[low+1])
+                    low++;
+                
+                while(low<high && nums[high]==nums[high-1])
+                    high--;
+                    
+                low++;
+                high--;     
+            }
+                else if(nums[low]+nums[high]>sum)
+                    high--;
+                else
+                    low++;
+            }
+                         }     }
+        return ansList;
+        
+}
+}
